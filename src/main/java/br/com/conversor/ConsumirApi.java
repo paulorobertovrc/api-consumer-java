98906990a8de237f1ff0b908d041cf8ea0f0871b
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-15 13:40:47 -0300

    Adicionados comentários ä classe ConsumirApi.java e convertido o valor da cotação retornado pelo JSON de string para double

--- src/main/java/br/com/conversor/ConsumirApi.java
+++ src/main/java/br/com/conversor/ConsumirApi.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class ConsumirApi {
 	public static void main(String[] args) throws IOException, ParseException {
+		// Consume the public API 
 		URL url = new URL("https://economia.awesomeapi.com.br/json/USD-BRL");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.connect();
 		
+		// Read the values returned from the API
 		StringBuilder s = new StringBuilder();
 		Scanner sc = new Scanner(url.openStream());
 		while(sc.hasNext()) {
@@ -25,11 +26,16 @@ public class ConsumirApi {
 		}
 		sc.close();		
 		
-		JSONParser parser = new JSONParser();
-		JSONArray json = (JSONArray) parser.parse(String.valueOf(s));
-		JSONObject cotacao = (JSONObject) json.get(0);
+		// Convert the returned values to JSON object 
+		JSONParser jParser = new JSONParser();
+		JSONArray jArray = (JSONArray) jParser.parse(String.valueOf(s));
+		JSONObject jObj = (JSONObject) jArray.get(0);
 		
-		System.out.println(cotacao.get("bid"));
+		// Get the value from the "bid" key, as explained in the API's documentation
+		String priceString = jObj.get("bid").toString();
+		
+		// Convert the price from string to double format
+		Double price = Double.valueOf(priceString);
+		System.out.println(price);
 	}
-
 }
